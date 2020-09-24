3d2752b2c42a1360891d6b8bc52742d9a77e282d
villivald <maxim.villivald@student.lab.fi>
2020-09-24 14:39:22 +0300

    last posts images/links in the Screens component + started doing media queries (500)

--- src/App.js
+++ src/App.js
@@ -4,86 +4,55 @@ import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 import Screens from "./components/Screens";
 import Timelines from "./components/Timeline";
-import { makeStyles, withStyles } from "@material-ui/core";
+import { withStyles } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import "antd/dist/antd.css";
 import { Progress } from "antd";
 import ChipCloud from "./components/ChipCloud";
-import Colors from "./constants/colors";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      width: theme.spacing(68),
-      height: theme.spacing(46),
-      backgroundColor: Colors.whitesmoke,
-    },
-  },
-}));
 
 const StyledPaper = withStyles({
   root: {
     fontFamily: "Mate SC, serif",
     backgroundColor: "",
     borderRadius: 5,
+    paddingBottom: 50,
   },
 })(Paper);
 
-const styles = {
-  timeline: {
-    display: "flex",
-    marginTop: "2%",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  timelines: {
-    marginLeft: "-10%",
-    marginTop: "2%",
-  },
-};
-
 const App = () => {
-  const classes = useStyles();
   return (
     <div className="main">
       <Menu />
       <Grid />
-      <div style={styles.timeline}>
-        <div style={styles.timelines}>
+      <div className="headers">
+        <h1>Timeline</h1>
+        <h1>Skills</h1>
+      </div>
+      <div className="timeline">
+        <div className="timelines">
           <Timelines />
         </div>
-        <div className={classes.root}>
-          <StyledPaper elevation={6}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <ChipCloud />
-            </div>
-            <div style={{ width: 100, marginLeft: 20, marginTop: 30 }}>
-              <Progress
-                percent={100}
-                size="small"
-                strokeColor={Colors.screens}
-              />{" "}
-              Russian
-              <Progress
-                percent={90}
-                size="small"
-                strokeColor={Colors.button}
-              />{" "}
-              Finnish
-              <Progress
-                percent={70}
-                size="small"
-                strokeColor={Colors.blog}
-              />{" "}
-              English
-            </div>
-          </StyledPaper>
+        <div className="chipWrapper">
+          <div className="chipper">
+            <StyledPaper elevation={6}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: 200,
+                }}
+              >
+                <ChipCloud />
+              </div>
+              <div style={{ width: 200, marginLeft: 20, marginTop: 30 }}>
+                <Progress percent={37.5} size="small" /> B.E. 2019 - 2023
+                (90/240 ECTS)
+                <Progress percent={100} size="small" /> M.A. 2011 - 2015 (300
+                ECTS)
+              </div>
+            </StyledPaper>
+          </div>
         </div>
       </div>
       <Screens />
--- src/components/Screens.js
+++ src/components/Screens.js
@@ -2,6 +2,9 @@ import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Colors from "../constants/colors";
+import logo8 from "../images/8.png";
+import logo7 from "../images/7.png";
+import logo6 from "../images/6.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,12 +12,13 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: "wrap",
     "& > *": {
       margin: theme.spacing(1),
-      width: theme.spacing(48),
-      height: theme.spacing(26),
+      width: theme.spacing(43),
+      height: theme.spacing(36),
     },
     justifyContent: "space-around",
     marginTop: 40,
     marginBottom: 50,
+    fontSize: 18,
   },
 }));
 
@@ -30,10 +34,40 @@ const StyledPaper = withStyles({
 const Screens = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <StyledPaper elevation={6} />
-      <StyledPaper elevation={6} />
-      <StyledPaper elevation={6} />
+    <div>
+      <h1>The Latest Blog Posts</h1>
+      <div className={classes.root}>
+        <StyledPaper elevation={6}>
+          <a href="https://create-react-app.com/8-university-zsh/">
+            <img
+              src={logo8}
+              width="350px"
+              height="290px"
+              alt="blog-post-8-logo"
+            />
+          </a>
+        </StyledPaper>
+        <StyledPaper elevation={6}>
+          <a href="https://create-react-app.com/7-react-native/">
+            <img
+              src={logo7}
+              width="350px"
+              height="290px"
+              alt="blog-post-7-logo"
+            />
+          </a>
+        </StyledPaper>
+        <StyledPaper elevation={6}>
+          <a href="https://create-react-app.com/6-javascript-methods-map-filter-reduce/">
+            <img
+              src={logo6}
+              width="350px"
+              height="290px"
+              alt="blog-post-6-logo"
+            />
+          </a>
+        </StyledPaper>
+      </div>
     </div>
   );
 };
