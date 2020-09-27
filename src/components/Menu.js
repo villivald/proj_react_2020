aa5cb84e89126411741572d5f279e8644eea8607
villivald <maxim.villivald@student.lab.fi>
2020-09-27 10:34:06 +0300

    fix safari media queries

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,9 @@
 import React from "react";
-import Colors from "../constants/colors";
 import Drop from "./Drop";
 import Toggle from "./Toggle";
 import logo from "../images/logo/103.png";
 
-const Menu = (props) => {
-  const changeTextColor = (e) => {
-    e.target.style.color = Colors.accent;
-  };
-  const setTextColorToDefault = (e) => {
-    e.target.style.color = "#333";
-  };
+const Menu = () => {
   const handleClick = () => {
     window.parent.location = window.parent.location.href;
   };
@@ -22,12 +15,7 @@ const Menu = (props) => {
           <div className="switch">
             <Toggle />
           </div>
-          <div
-            className="header"
-            onMouseOver={changeTextColor}
-            onMouseLeave={setTextColorToDefault}
-            onClick={handleClick}
-          >
+          <div className="header" onClick={handleClick}>
             <img className="logo" src={logo} alt="Logo" width="150px" />
           </div>
           <div className="drop">
