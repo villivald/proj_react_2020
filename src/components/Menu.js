3e5f38b0c79805df99e0492f9bea38e2b1db003f
villivald <maxim.villivald@student.lab.fi>
2020-10-02 22:05:47 +0300

    pop motion logo rotation on hover

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,10 +2,15 @@ import React from "react";
 import Toggle from "./Toggle";
 import logo from "../images/logo/103.png";
 import Dropdown from "../components/Dropdown";
+import { Frame } from "framer";
 
 const Menu = () => {
   const handleClick = () => {
     window.parent.location = window.parent.location.href;
+  };
+  const variants = {
+    variantA: { scale: 1, rotate: 0 },
+    variantB: { scale: 1.2, rotate: 90 },
   };
 
   return (
@@ -16,7 +21,16 @@ const Menu = () => {
             <Toggle />
           </div>
           <div className="header" onClick={handleClick}>
-            <img className="logo" src={logo} alt="Logo" width="150px" />
+            <Frame
+              initial={"variantA"}
+              whileHover={"variantB"}
+              variants={variants}
+              size={150}
+              radius={30}
+              background={"hsla(0, 100%, 50%, 0)"}
+            >
+              <img className="logo" src={logo} alt="Logo" width="150px" />
+            </Frame>
           </div>
           <div className="drop">
             <Dropdown />
