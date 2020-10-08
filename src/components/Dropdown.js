2cd9ce0723bfc67a1aef5a1fec728d4566405fe7
Adham Mohamed <adhamacc1@gmail.com>
2020-10-09 01:20:31 +0200

    Added Dropdown Navigation

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 
 const DropDown = () => {
   return (
     <DropdownButton id="dropdown-basic-button" variant="info" title="Menu">
-      <Dropdown.Item href="http://google.com">Not</Dropdown.Item>
-      <Dropdown.Divider />
-      <Dropdown.Item href="https://www.linkedin.com/in/maxim-villivald-4b1b311a3/">
-        Yet
+      <Dropdown.Item>
+        <Link to="/">Home Page</Link>
       </Dropdown.Item>
       <Dropdown.Divider />
-      <Dropdown.Item href="#/action-3">Ready</Dropdown.Item>
+      <Dropdown.Item>
+        <Link to="/first">First Page</Link>
+      </Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item>
+        <Link to="/second">Second Page</Link>
+      </Dropdown.Item>
     </DropdownButton>
   );
 };
---
+++ src/pages/second.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Menu from "../components/Menu";
+import Footer from "../components/Footer";
+
+const Second = () => {
+  return (
+    <div className="main">
+      <Menu />
+      <h1>Second Page</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Second;
