2038e0e6d242272fec6e12c630de7fd9f3b3daf9
villivald <maxim.villivald@student.lab.fi>
2020-10-10 16:51:54 +0300

    updated blog posts and twitter link, deleted old png pics

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -2,9 +2,9 @@ import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Colors from "../constants/colors";
+import logo9 from "../images/9.webp";
 import logo8 from "../images/8.webp";
 import logo7 from "../images/7.webp";
-import logo6 from "../images/6.webp";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -38,6 +38,17 @@ const Screens = () => {
       <h1>The Latest Blog Posts</h1>
       <div className={classes.root}>
         <StyledPaper elevation={6}>
+          <a href="https://create-react-app.com/new-website/">
+            <img
+              className="image"
+              src={logo9}
+              width="350px"
+              height="290px"
+              alt="blog-post-9-logo"
+            />
+          </a>
+        </StyledPaper>
+        <StyledPaper elevation={6}>
           <a href="https://create-react-app.com/8-university-zsh/">
             <img
               className="image"
@@ -56,17 +67,6 @@ const Screens = () => {
               width="350px"
               height="290px"
               alt="blog-post-7-logo"
-            />
-          </a>
-        </StyledPaper>
-        <StyledPaper elevation={6}>
-          <a href="https://create-react-app.com/6-javascript-methods-map-filter-reduce/">
-            <img
-              className="image"
-              src={logo6}
-              width="350px"
-              height="290px"
-              alt="blog-post-6-logo"
             />
           </a>
         </StyledPaper>
