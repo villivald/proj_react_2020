49aadbf9d60cc02be4c02eb3cc805c481201572a
villivald <maxim.villivald@gmail.com>
2020-12-02 22:43:35 +0200

    add links to courses page, divide to two parts

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -10,6 +10,7 @@ const Second = () => {
     <div className="main">
       <Menu menuStyle="menu" ButtonDisplay="none" />
       <h1 className="contacts">Courses & Certifications</h1>
+      <h2 className="h2-courses">Completed</h2>
       <div className="courses">
         <ul>
           <li className="course course-li">
@@ -20,7 +21,9 @@ const Second = () => {
               height="45"
               width="45"
             />
-            Linux & CLI, Brian Holt - 06.09.20
+            <a href="https://frontendmasters.com/courses/linux-command-line/">
+              Complete Intro to Linux and the Command-Line - 06.09.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -30,7 +33,9 @@ const Second = () => {
               height="45"
               width="45"
             />
-            Getting a Front-End Developer Job - 15.09.20
+            <a href="https://frontendmasters.com/courses/getting-a-job/">
+              Getting a Front-End Developer Job - 15.09.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -40,7 +45,9 @@ const Second = () => {
               height="35,5"
               width="50"
             />
-            Responsive Web Design - 25.10.20
+            <a href="https://freecodecamp.org/certification/villivald/responsive-web-design">
+              Responsive Web Design - 25.10.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -50,7 +57,9 @@ const Second = () => {
               height="45"
               width="45"
             />
-            CSS In-Depth - 02.11.20
+            <a href="https://frontendmasters.com/courses/css-in-depth-v2/">
+              CSS In-Depth - 02.11.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -60,7 +69,9 @@ const Second = () => {
               height="45"
               width="45"
             />
-            Michigan University - Internet History - 09.11.20
+            <a href="https://www.coursera.org/account/accomplishments/certificate/XXH88UHKWCNW">
+              Michigan University - Internet History - 09.11.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -70,7 +81,9 @@ const Second = () => {
               height="45"
               width="45"
             />
-            Design Systems with Storybook & React - 16.11.20
+            <a href="https://frontendmasters.com/courses/design-systems/">
+              Design Systems with React & Storybook - 16.11.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -80,7 +93,9 @@ const Second = () => {
               height="45"
               width="45"
             />
-            Visual Studio Code Can Do That? - 23.11.20
+            <a href="https://frontendmasters.com/courses/customize-vs-code/">
+              Visual Studio Code Can Do That? - 23.11.20
+            </a>
           </li>
           <li className="course course-li">
             <img
@@ -90,7 +105,34 @@ const Second = () => {
               height="35,5"
               width="50"
             />
-            JavaScript Algorithms and Data Structures - 02.12.20
+            <a href="https://freecodecamp.org/certification/villivald/javascript-algorithms-and-data-structures">
+              JavaScript Algorithms and Data Structures - 02.12.20
+            </a>
+          </li>
+          <h2 className="h2-courses">Ongoing</h2>
+          <li className="course course-li">
+            <img
+              className="icons course-icons"
+              src={fm}
+              alt="frontend masters logo"
+              height="45"
+              width="45"
+            />
+            <a href="https://frontendmasters.com/courses/fullstack-v2/">
+              Full Stack for Front-End Engineers
+            </a>
+          </li>
+          <li className="course course-li">
+            <img
+              className="icons course-icons"
+              src={fcc}
+              alt="freecodecamp logo"
+              height="35,5"
+              width="50"
+            />
+            <a href="https://www.freecodecamp.org/learn/">
+              Front End Libraries
+            </a>
           </li>
         </ul>
       </div>
