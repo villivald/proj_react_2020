7cc16d624c72bad390dee720182f0ed320924c36
villivald <maxim.villivald@gmail.com>
2020-12-21 17:14:00 +0200

    lazy loading example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
-import Uses from "./pages/uses";
-import Courses from "./pages/courses";
-import Books from "./pages/books";
+// import About from "./pages/about";
+// import Contact from "./pages/contact";
+// import Uses from "./pages/uses";
+// import Courses from "./pages/courses";
+// import Books from "./pages/books";
+
+const About = lazy(() => import("./pages/about"));
+const Uses = lazy(() => import("./pages/uses"));
+const Contact = lazy(() => import("./pages/contact"));
+const Courses = lazy(() => import("./pages/courses"));
+const Books = lazy(() => import("./pages/books"));
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/contact" exact component={Contact} />
-        <Route path="/uses" exact component={Uses} />
-        <Route path="/courses" exact component={Courses} />
-        <Route path="/books" exact component={Books} />
+        <Suspense fallback={<h1>loading...</h1>}>
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/uses" exact component={Uses} />
+          <Route path="/courses" exact component={Courses} />
+          <Route path="/books" exact component={Books} />
+        </Suspense>
       </Switch>
     </Router>
   );
