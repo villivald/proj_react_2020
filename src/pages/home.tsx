6b72799cd7e1ab7fd9ee1402b286134d824a2012
villivald <maxim.villivald@gmail.com>
2021-02-17 18:11:25 +0200

    add lazy loading to Screens component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,14 @@
-import React, { useState, FunctionComponent } from "react";
+import React, { useState, lazy, Suspense, FunctionComponent } from "react";
 import Grid from "../components/Grid";
 import Menu from "../components/Menu";
 import Footer from "../components/Footer";
-import Screens from "../components/Screens";
 import Stats from "../components/Stats";
 import night from "../images/night.webp";
 import lamp from "../images/lamp.png";
 import Colors from "../constants/colors";
 import Arrow from "../components/Arrow";
+import BeatLoader from "react-spinners/BeatLoader";
+const Screens = lazy(() => import("../components/Screens"));
 
 const Home: FunctionComponent = () => {
   const [mainStyle, setMainStyle] = useState(false);
@@ -39,7 +40,22 @@ const Home: FunctionComponent = () => {
         ChipColors={mainStyle ? "#DDDFE3" : Colors.header}
         StatsTextColor={mainStyle ? "white" : Colors.almostBlack}
       />
-      <Screens statsHeaders={mainStyle ? "tesla-alt" : "tesla"} />
+      <Suspense
+        fallback={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 100,
+            }}
+          >
+            <BeatLoader color={Colors.blog} size={30} loading={true} />
+          </div>
+        }
+      >
+        <Screens statsHeaders={mainStyle ? "tesla-alt" : "tesla"} />
+      </Suspense>
       <Footer
         footerStyle={mainStyle ? "footer-alt" : "footer"}
         footerTextStyle={mainStyle ? "a-alt" : "a"}
