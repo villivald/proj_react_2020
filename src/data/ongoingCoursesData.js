b9246f62a2108f6345c6b9603ce9b0adb1d990eb
villivald <maxim.villivald@gmail.com>
2021-06-07 10:12:18 +0300

    delete unused code

--- src/data/ongoingCoursesData.js
+++ src/data/ongoingCoursesData.js
@@ -1,5 +1,4 @@
 import helsinki from "../images/helsinki.webp";
-import react2025 from "../images/react2025.svg";
 
 const ongoingCourses = [
   {
@@ -9,14 +8,6 @@ const ongoingCourses = [
     width: 45,
     link: "https://fullstackopen.com/en/",
     name: "Full Stack Open 2021",
-  },
-  {
-    pic: react2025,
-    alt: "react 2025 logo",
-    height: 45,
-    width: 45,
-    link: "https://react2025.com/",
-    name: "React 2025",
   },
 ];
 
