8c6b1a97c47a28ff08aa0f2c935c82efa00eb1b1
villivald <maxim.villivald@gmail.com>
2021-06-24 14:18:26 +0300

    add new courses & small fixes

--- src/data/ongoingCoursesData.js
+++ src/data/ongoingCoursesData.js
@@ -1,4 +1,6 @@
 import helsinki from "../images/helsinki.webp";
+import fcc from "../images/fccdark.webp";
+import fm from "../images/fm.jpg";
 
 const ongoingCourses = [
   {
@@ -8,6 +10,22 @@ const ongoingCourses = [
     width: 45,
     link: "https://fullstackopen.com/en/",
     name: "Full Stack Open 2021",
+  },
+  {
+    pic: fm,
+    alt: "frontend masters logo",
+    height: 45,
+    width: 45,
+    link: "https://frontendmasters.com/courses/complete-intro-containers/",
+    name: "Complete Intro to Containers",
+  },
+  {
+    pic: fcc,
+    alt: "freecodecamp logo",
+    height: 35.5,
+    width: 50,
+    link: "https://www.youtube.com/watch?v=ulprqHHWlng",
+    name: "AWS Basics",
   },
 ];
 
