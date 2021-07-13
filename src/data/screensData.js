f6334cb1f17b7a8b9b845bd0c33a710151529f5c
villivald <maxim.villivald@gmail.com>
2021-07-13 10:27:24 +0300

    add new blog post pic & fix post links

--- src/data/screensData.js
+++ src/data/screensData.js
@@ -1,22 +1,22 @@
+import logo23 from "../images/23.webp";
 import logo22 from "../images/22.webp";
 import logo21 from "../images/21.webp";
-import logo20 from "../images/20.webp";
 
 const screens = [
   {
-    url: "https://create-react-app.com/top-10-web-dev-tools-05-2021/",
+    url: "https://www.create-react-app.com/posts/2021-07-12-top-10-web-dev-tools-07-2021/",
+    id: 23,
+    logo: logo23,
+  },
+  {
+    url: "https://www.create-react-app.com/posts/2021-06-15-top-10-web-dev-tools-06-2021/",
     id: 22,
     logo: logo22,
   },
   {
-    url: "https://create-react-app.com/15-most-valuable-github-repos-in-2021/",
+    url: "https://www.create-react-app.com/posts/2021-05-10-top-10-web-dev-tools-05-2021/",
     id: 21,
     logo: logo21,
-  },
-  {
-    url: "https://create-react-app.com/top-10-web-dev-tools-04-2021/",
-    id: 20,
-    logo: logo20,
   },
 ];
 
