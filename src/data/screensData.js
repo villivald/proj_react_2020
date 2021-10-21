60de8e138e61a5a739cbc0f4ec8d2f57fefdb481
villivald <maxim.villivald@gmail.com>
2021-10-21 17:26:12 +0300

    add new blog post

--- src/data/screensData.js
+++ src/data/screensData.js
@@ -1,8 +1,13 @@
+import logo26 from "../images/26.webp";
 import logo25 from "../images/25.webp";
 import logo24 from "../images/24.webp";
-import logo23 from "../images/23.webp";
 
 const screens = [
+  {
+    url: "https://www.create-react-app.com/posts/2021-10-19-top-10-web-dev-tools-10-2021/",
+    id: 26,
+    logo: logo26,
+  },
   {
     url: "https://www.create-react-app.com/posts/2021-09-16-top-10-web-dev-tools-09-2021/",
     id: 25,
@@ -12,11 +17,6 @@ const screens = [
     url: "https://www.create-react-app.com/posts/2021-08-17-top-10-web-dev-tools-08-2021/",
     id: 24,
     logo: logo24,
-  },
-  {
-    url: "https://www.create-react-app.com/posts/2021-07-12-top-10-web-dev-tools-07-2021/",
-    id: 23,
-    logo: logo23,
   },
 ];
 
