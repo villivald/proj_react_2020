35d2df22e666cc05f1fa0bc73e90e34f3a936762
Maxim Villivald <maxim.villivald@gmail.com>
2022-04-15 20:07:10 +0300

    add new blog post to main page

--- src/data/screensData.js
+++ src/data/screensData.js
@@ -1,8 +1,13 @@
+import logo29 from "../images/29.webp";
 import logo28 from "../images/28.webp";
 import logo27 from "../images/27.webp";
-import logo26 from "../images/26.webp";
 
 const screens = [
+  {
+    url: "https://www.create-react-app.com/posts/2022-04-03-how-i-got-my-first-job-in-it/",
+    id: 29,
+    logo: logo29,
+  },
   {
     url: "https://www.create-react-app.com/posts/2021-12-26-top-10-web-dev-tools-2021/",
     id: 28,
@@ -12,11 +17,6 @@ const screens = [
     url: "https://www.create-react-app.com/posts/2021-11-29-top-10-web-dev-tools-11-2021/",
     id: 27,
     logo: logo27,
-  },
-  {
-    url: "https://www.create-react-app.com/posts/2021-10-19-top-10-web-dev-tools-10-2021/",
-    id: 26,
-    logo: logo26,
   },
 ];
 
